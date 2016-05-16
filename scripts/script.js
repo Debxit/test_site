a2a50eebce180959c2d914e61e1090a0650018b7
OlgaK <debxit@gmail.com>
2016-05-16 17:12:44 +0300

    SVG
    Added svg figure and spin animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,35 @@
 
 var main = function() {
-	  $('.article').click(function() {
-    $('.article').removeClass('current');
-    $('.description').hide();
+	$('.article').click(function() {
+   // $('.article').removeClass('current');
+   // $('.description').hide();
 
-    $(this).addClass('current');
-    $(this).children('.description').show();
+    $(this).toggleClass('current');
+    $(this).children('.description').toggle();
   });
-
-  $(document).keypress(function(event) {
-    if(event.which === 111) {  //key n
+/*
+  $(document).keyup(function(event) {
+    if(event.which === 40) {  //key down
       $('.description').hide();
 
       $('.current').children('.description').show();
     }
 
-    else if(event.which === 110) { //key o
+ else if(event.which === 38) {  //key up
+      $('.description').hide();
+
+      $('.current').parent('.description').show();
+    }
+});
+ $(document).keypress(function(event) {
+    if(event.which === 13) { //key enter
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.next();
+      
+      currentArticle.removeClass('current');
+      nextArticle.addClass('current');
+    }
+       else if(event.which === 32) { //key space
       var currentArticle = $('.current');
       var nextArticle = currentArticle.next();
       
@@ -23,7 +37,7 @@ var main = function() {
       nextArticle.addClass('current');
     }
   });
-
+*/
   $("#foto").click(function(){
 		if ($(this).text()=="Not see me"){
         $(this).text("See me");
